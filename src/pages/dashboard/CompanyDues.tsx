ba0a4bf8b4081bb0b9656e11b45094a575ea43d5
Ismail Hasan Sarker <99243037+ismail-asl@users.noreply.github.com>
2023-02-23 13:30:55 +0600

    fixed dynamic logo fetching issue and due chart view condition

--- src/pages/dashboard/CompanyDues.tsx
+++ src/pages/dashboard/CompanyDues.tsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import {Card, Row, Col, ProgressBar } from 'react-bootstrap';
+import { Card, Row, Col, ProgressBar } from 'react-bootstrap';
 
 // componets
 
-const CompanyDues = ({summaryList, scurrency}:any) => {
+const CompanyDues = ({ summaryList, scurrency }: any) => {
     return (
         <>
             <Card>
                 <Card.Body>
-                    
+
                     <h4 className="header-title">Clients Due</h4>
 
-                    {summaryList?.clients_dues?.map((due:any)=>{
-                        return (
-                            <>
-                                <div className="mt-4">
-                                    <h6 className="text-uppercase">
-                                        {due?.contact_id__name} <span className="float-end">{scurrency?.symbol}{due?.net_due_amount}</span>
-                                    </h6>
-    
-                                    <ProgressBar
-                                        now={due?.percentage}
-                                        className="progress m-0"
-                                        label={due?.percentage + " %"}
-                                        // visuallyHidden
-                                        variant="qorum"
-                                    />
-                                </div>
-                            </>
-                        )
-                    }
-                     
-                    )}
-                    
+                    {summaryList?.clients_dues?.length > 0 ?
+
+                        summaryList?.clients_dues?.map((due: any) => {
+                            return (
+                                <>
+                                    <div className="mt-4">
+                                        <h6 className="text-uppercase">
+                                            {due?.contact_id__name} <span className="float-end">{scurrency?.symbol}{due?.net_due_amount}</span>
+                                        </h6>
+
+                                        <ProgressBar
+                                            now={due?.percentage}
+                                            className="progress m-0"
+                                            label={due?.percentage + " %"}
+                                            // visuallyHidden
+                                            variant="qorum"
+                                        />
+                                    </div>
+                                </>
+                            )
+                        }
+
+                        ) : "No client due found"}
 
 
-                    
                 </Card.Body>
             </Card>
         </>
