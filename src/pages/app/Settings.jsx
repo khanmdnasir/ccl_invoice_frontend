8c8dd891e368c0c57812d26ae403888c74b3eb63
Nasir Khan <nasir@aslgroup.com.bd>
2023-06-18 17:47:53 +0600

    settings update

--- src/pages/app/Settings.jsx
+++ src/pages/app/Settings.jsx
@@ -8,25 +8,6 @@ const Settings = () => {
       <div className="row">
         <div className="col-md-6 settings_page">
           <h6>General</h6>
-
-          <Link to="/app/users">
-            <div className="card">
-              <h4>Users</h4>
-              <p>Add, Remove & modify users of this organisation</p>
-            </div>
-          </Link>
-          <Link to="/app/roles">
-            <div className="card">
-              <h4>Roles</h4>
-              <p>Address, Logo, Design </p>
-            </div>
-          </Link>
-          <Link to="/app/currency">
-            <div className="card">
-              <h4>Currencies</h4>
-              <p>Address, Logo, Design </p>
-            </div>
-          </Link>
           <Link to="/app/company_settings">
             <div
               className="card"
@@ -38,13 +19,32 @@ const Settings = () => {
               <p>Address, Logo & basic financial information </p>
             </div>
           </Link>
+          <Link to="/app/users">
+            <div className="card">
+              <h4>Users</h4>
+              <p>Add, Remove & modify users of this organisation</p>
+            </div>
+          </Link>
+          <Link to="/app/roles">
+            <div className="card">
+              <h4>Roles</h4>
+              <p>Add,modify roles for users </p>
+            </div>
+          </Link>
+          <Link to="/app/currency">
+            <div className="card">
+              <h4>Currencies</h4>
+              <p>Add, modify currency for this organisation</p>
+            </div>
+          </Link>
+          
         </div>
         <div className="col-md-6 settings_page">
           <h6>Feature</h6>
           <Link to="/app/chart_of_accounts">
             <div className="card border-b">
               <h4>Chart of accounts</h4>
-              <p>Add, Remove & modify users of this organisation</p>
+              <p>Add chart of account of this organisation</p>
             </div>
           </Link>
           <Link to="/app/key_account_manager">
@@ -55,7 +55,7 @@ const Settings = () => {
               }}
             >
               <h4>Key account manager</h4>
-              <p>Address, Logo, Design </p>
+              <p>Add key account manager of this organisation</p>
             </div>
           </Link>
         </div>
